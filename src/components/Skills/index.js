859724f611a8cdd572182136bd603e1f96c8555c
Gabriel <gabrielmtvp@gmail.com>
2023-04-24 00:50:00 +0200

    Added some changes css and refactoring

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,16 +1,23 @@
+import { useState } from 'react'
+
 import './index.scss'
-import { faHtml5, faJs, faReact, faAmazon, faGoogle, 
-    faJava, faNodeJs, faGithub, faDocker, faCss3Alt, faSass, faMailchimp } from '@fortawesome/free-brands-svg-icons'
+
 import nextJs from '../../assets/images/nextjs.png'
 import graphQl from '../../assets/images/graphql.png'
 import mongoDb from '../../assets/images/mongodb.png'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import cell from '../../assets/cell2.png'
-import { useState } from 'react'
+
+import { faHtml5, faJs, faReact, faAmazon, faGoogle, 
+    faJava, faNodeJs, faGithub, faDocker, faCss3Alt, faSass, faMailchimp } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Skills = () => {
 
     const [txtButton, setTxtButton] = useState('frontend');
+
+    const handleSkillsTab = (tab) => {
+        return txtButton === tab ? 'active' : '';
+    }
     
     return (
         <section className="container-skills" id="skills">
@@ -19,23 +26,17 @@ const Skills = () => {
             </div>
 
             <div className='skills' data-aos="zoom-in">
-                <div className={`${txtButton === "frontend" ? 'activeImage3d' : 'hiddenImage3d'}`}>
-                    <img src={cell}  />
-                </div>
-                <div className={`${txtButton === "backend" ? 'activeImage3d' : 'hiddenImage3d'}`}>
-                    <img src={cell}  />
-                </div>
-                <div className={`${txtButton === "other" ? 'activeImage3d' : 'hiddenImage3d'}`}>
-                    <img src={cell}  />
+                <div className={'activeImage3d'}>
+                    <img src={cell} alt='mobile'  />
                 </div>
                 <div className='containerPanelsSkills'>
                     <div className='buttonsSkills'>
-                        <button onClick={() => setTxtButton('frontend')}>Front-end</button>
-                        <button onClick={() => setTxtButton('backend')}>Back-end</button>
-                        <button onClick={() => setTxtButton('other')}>Others</button>
+                        <button onClick={() => setTxtButton('frontend')} className={`${handleSkillsTab('frontend')}`} >Front-end</button>
+                        <button onClick={() => setTxtButton('backend')} className={`${handleSkillsTab('backend')}`} >Back-end</button>
+                        <button onClick={() => setTxtButton('other')} className={`${handleSkillsTab('other')}`}>Others</button>
                     </div>
                     <div className='panelsSkills'>
-                        <div className={`${txtButton === "frontend" ? 'active' : ''}`}>
+                        <div className={`${handleSkillsTab('frontend')}`}>
                             <div className='image3d'>
                                 <FontAwesomeIcon icon={faHtml5} size="5x" style={{color:"orange"}}/>
                                 <span>HTML5</span>
@@ -60,7 +61,7 @@ const Skills = () => {
                                 <span>Sass</span>
                             </div>
                         </div>
-                        <div className={`${txtButton === "backend" ? 'active' : ''}`}>
+                        <div className={`${handleSkillsTab('backend')}`}>
                             <div className='image3d'>
                                 <FontAwesomeIcon icon={faNodeJs} size="6x" style={{color:"green"}} />
                                 <span>NodeJs</span>
@@ -81,7 +82,7 @@ const Skills = () => {
                                 <span>Java</span>
                             </div>
                         </div>
-                        <div className={`${txtButton === "other" ? 'active' : ''}`}>
+                        <div className={`${handleSkillsTab('other')}`}>
                             <div className='image3d'>
                                 <img src={nextJs} width={100} alt="nextjs" />
                                 <span>NextJs</span>
@@ -114,83 +115,9 @@ const Skills = () => {
                         </div>
                     </div>
                 </div>
-                {/* <div className='image3d'>
-                    <FontAwesomeIcon icon={faHtml5} size="6x" style={{color:"orange"}}/>
-                    <span>HTML5</span>
-                </div>
-
-                <div className='image3d'>
-                    <FontAwesomeIcon icon={faCss3Alt} size="6x" style={{color:"lightblue"}} />
-                    <span>CSS3</span>
-                </div>
-
-                <div className='image3d'>
-                    <FontAwesomeIcon icon={faJs} size="6x" style={{color:"yellow"}} />
-                    <span>JavaScript</span>
-                </div>
-
-                <div className='image3d'>
-                    <FontAwesomeIcon icon={faReact} size="6x" style={{color:"lightblue"}} />
-                    <span>ReactJs</span>
-                </div>
-
-                <div className='image3d'>
-                    <img src={nextJs} width={100} alt="nextjs" />
-                    <span>NextJs</span>
-                </div>
-
-                <div className='image3d'>
-                    <img src={graphQl} width={70} height={70} alt="graphql"  />
-                    <span>GraphQL</span>
-                </div>
-
-                <div className='image3d'>
-                    <img src={mongoDb} width={20} height={70} alt="mongodb"  />
-                    <span>MongoDB</span>
-                </div>
-
-                <div className='image3d'>
-                    <FontAwesomeIcon icon={faAmazon} size="6x" style={{color:"orange"}} />
-                    <span>Amazon Cloud</span>
-                </div>
-
-                <div className='image3d'>
-                    <FontAwesomeIcon icon={faGoogle} size="6x" style={{color:"lightyellow"}} />
-                    <span>Google Cloud</span>
-                </div>
-
-                <div className='image3d'>
-                    <FontAwesomeIcon icon={faJava} size="6x" style={{color:"lightblue"}} />
-                    <span>Java</span>
-                </div>
-
-                <div className='image3d'>
-                    <FontAwesomeIcon icon={faNodeJs} size="6x" style={{color:"green"}} />
-                    <span>NodeJs</span>
-                </div>
-
-                <div className='image3d'>
-                    <FontAwesomeIcon icon={faGithub} size="6x" style={{color:"white"}} />
-                    <span>GitHub</span>
-                </div>
-
-                <div className='image3d'>
-                    <FontAwesomeIcon icon={faDocker} size="6x" style={{color:"lightblue"}} />
-                    <span>Docker</span>
-                </div>
-                
-                <div className='image3d'>
-                    <FontAwesomeIcon icon={faSass} size="6x" style={{color:"pink"}} />
-                    <span>Sass</span>
-                </div>
-                <div className='image3d'>
-                    <FontAwesomeIcon icon={faMailchimp} size="6x" style={{color:"yellow"}} />
-                    <span>MailChimp</span>
-                </div> */}
             </div>
-           
         </section>
-        );
-    }
+    );
+}
 
 export default Skills;
--- src/components/Timeline/index.js
+++ src/components/Timeline/index.js
@@ -35,28 +35,6 @@ const Timeline = () => {
                 <p>Timeline</p>
                 <h1>Companies that I've been experience.</h1>
             </div>
-            {/* <div class="flip flip-vertical">
-                <div class="front">
-                    <h1 class="text-shadow">Software Engineer, ClusterStack</h1>
-                    <p>JUN 2022 - Present</p>
-                </div>
-                <div class="back">
-                    <h1>Software Engineer</h1>
-                    <p>As Software Engineer I'm working to help the company build a system that have as mainly idea bring more information and opportunities to the person that want immigrate to another country.</p>
-                </div>
-            </div>
-            <div class="flip flip-vertical">
-                <div class="front">
-                    <h1 class="text-shadow">Java Software Engineer, Freedom System</h1>
-                    <p>AUG 2015 - DEC 2021</p>
-                </div>
-                <div class="back">
-                    <h2>Java Software Enginner</h2>
-                    <p>At Freedom System I was worked as Java Software developer, and as some tasks that I had to perform being start new projects, talk with their clients, get the requirements of the project and build projects with clean code and excellence using the languages of the company.
-Sometimes handling with problems and having to find the best approach to solve them, giving to the client the best result and experience.
-In different cases I had to learn new languages, methods and external API's to improve the project experience.</p>
-                </div>
-            </div> */}
 
             <Accordion.Root className="AccordionRoot" type="single" defaultValue="item-1" collapsible data-aos="fade-right">
                 <Accordion.Item className="AccordionItem" value="item-1">
